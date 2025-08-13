8bed4f04a1c57ec6123a09c371119fecdc601c82
Haseeb Khan <hk2863262@gmail.com>
2025-08-13 14:58:12 +0500

    NODEMON + SETTING UP ROUTES

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,29 @@ import express, { request, response } from "express";
 const app = express();
 
 app.get("/api/notes", (request, response) => {
-  response.status(200).send("You have accessed the /api/notes api endpoint");
+  response
+    .status(200)
+    .send("You have accessed the /api/notes api endpoint with a GET request");
+});
+
+app.post("/api/notes", (request, response) => {
+  response.status(201).json({
+    message:
+      "You have accessed the /api/notes api endpoint with a POST request",
+  });
+});
+
+app.put("/api/notes/:id", (request, response) => {
+  response.status(200).json({
+    message: "You have accessed the /api/notes api endpoint with a put request",
+  });
+});
+
+app.post("/api/notes/:id", (request, response) => {
+  response.status(201).json({
+    message:
+      "You have accessed the /api/notes api endpoint with a DELETE request",
+  });
 });
 
 app.listen(3000, () => {
